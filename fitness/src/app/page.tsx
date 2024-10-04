6f9a944130bbabdc76ecb6c4cab56b6f5855b975
Jefin John <jefinjohn3273@gmail.com>
2024-10-04 06:10:28 +0530

    last commit for the day

--- fitness/src/app/page.tsx
+++ fitness/src/app/page.tsx
@@ -1,28 +1,31 @@
-import HomePage from "@/Components/HomePage";
+import HomePage from "../components/HomePage";
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function Home() {
   return (
-    <HomePage/>
-    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 flex flex-col justify-center items-center text-white">
-      <h1 className="text-4xl font-bold mb-6">Welcome to FitConnect</h1>
-      <p className="text-lg mb-8">
-        Join our community and track your fitness journey.
-      </p>
-      <div className="flex space-x-4">
-        <Link href="/login">
-          <Button className="bg-white text-blue-500 hover:bg-blue-600 hover:text-white">
-            Login
-          </Button>
-        </Link>
-        <Link href="/signup">
-          <Button className="bg-white text-blue-500 hover:bg-blue-600 hover:text-white">
-            Sign Up
-          </Button>
-        </Link>
+    <>
+      <HomePage/>
+      <div className="min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 flex flex-col justify-center items-center text-white">
+        <h1 className="text-4xl font-bold mb-6">Welcome to FitConnect</h1>
+        <p className="text-lg mb-8">
+          Join our community and track your fitness journey.
+        </p>
+        <div className="flex space-x-4">
+          <Link href="/login">
+            <Button className="bg-white text-blue-500 hover:bg-blue-600 hover:text-white">
+              Login
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button className="bg-white text-blue-500 hover:bg-blue-600 hover:text-white">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
